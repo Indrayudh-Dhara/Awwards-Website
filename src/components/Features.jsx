fbc555437b396612193db34d6394a87d2442efa2
Indrayudh Dhara <dharaindrayudh16@gmail.com>
2025-08-29 17:25:04 +0530

    Implemented Footer, Contact , Features . Complete Project Till Now

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { TiLocationArrow } from "react-icons/ti";
+
+const BentoTilt = ({children , className=''})=>{
+    const[transformStyle , setTransformStyle] = useState('');
+    const itemRef = useRef();
+    const handleMouseMove=(e)=>{
+        if(!itemRef.current){
+            return;
+        }
+        const {left , top , width , height} = itemRef.current.getBoundingClientRect();
+
+        const relativeX = (e.clientX - left) / width;
+        const relativeY = (e.clientY - top) / height;
+
+        const tiltX = (relativeY - 0.5) * 10;
+        const tiltY = (relativeX - 0.5) * -10;
+
+        const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(0.95 , 0.95 , 0.95)`;
+
+        setTransformStyle(newTransform);
+    }
+    const handleMouseLeave=()=>{
+        setTransformStyle('');
+    }
+    return (
+        <div className={className} ref={itemRef} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{
+        transform: transformStyle,
+        transition: 'transform 0.2s ease-out', // 👈 smooth easing
+      }}>
+            {children}
+        </div>
+    )
+}
 
 const BentoCard = ({src , title , description , isComingSoon})=>{
     return (
         <div className="relative size-full">
             <video src={src} loop autoPlay muted className="absolute left-0 top-0 size-full object-cover object-center"/>
-            <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+            <div className="relative z-10 flex size-full flex-col p-5 text-blue-50">
                 <h1 className="bento-title special-font">
                     {title}
                 </h1>
+                {description && (
+                        <p className="mt-3 max-w-80 text-xs md:text-base">
+                            {description}
+                        </p>
+                )}
             </div>
         </div>
     )
@@ -22,10 +60,34 @@ const Features = () => {
                     Explore the Zentry Universe
                 </p>
                 <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
-Immerse yourself in an IP-rich product universe where players, agentic AI and blockchain lead the new economic paradigm.                </p>
+Immerse yourself in an IP-rich product universe where players, agentic AI and blockchain lead the new economic paradigm.
+                </p>
             </div>
-            <div className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
-                <BentoCard src="videos/feature-1.mp4" title={<>radia<b>n</b>t</> }description="the game of games app transforming moments across Web2 & Web3 into rewards" isComingSoon/>
+            <BentoTilt className="border-hsla  relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+                <BentoCard src="videos/feature-1.mp4" title={<>radia<b>n</b>t</> }description="The game of games app transforming moments across Web2 & Web3 into rewards" isComingSoon/>
+            </BentoTilt>
+            <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+                <BentoTilt className="bento-tilt_1  md:col-span-1 md:row-span-2">
+                    <BentoCard src="videos/feature-2.mp4" title={<>zig<b>m</b>a</>} description="The NFT collection merging Zentry's IP, AI, and gaming-pushing the boundaries of NFT innovation"/>
+                </BentoTilt>
+                <BentoTilt className=" bento-tilt_1  ms-32  md:ms-0">
+                    <BentoCard src="videos/feature-3.mp4"  title={<>n<b>e</b>xus</>} description="The metagame portal uniting humans & AI to play, compete and earn" />
+                </BentoTilt>
+                <BentoTilt className="bento-tilt_1  me-14 md:col-span-1 md:me-0">
+                    <BentoCard src="videos/feature-4.mp4"  title={<>az<b>u</b>l</>} description="The agent of agents elevating agentic AI experience to be more fun and productive" />
+                </BentoTilt>
+                <BentoTilt className="bento-tilt_2">
+                    <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+                        <h1 className="bento-title special-font max-w-64 text-black">
+
+                            M<b>o</b>re Co<b>m</b>ing S<b>o</b>on
+                         </h1>
+                            <TiLocationArrow className="m-5 scale-[5] self-end"/>   
+                    </div>
+                </BentoTilt>
+                <BentoTilt className="bento-tilt_2">
+                    <video src="videos/feature-5.mp4" loop muted autoPlay className="size-full object-cover object-center"/>
+                </BentoTilt>
             </div>
 
         </div>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FaGithub , FaLinkedin  } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+
+const links = [
+    {href : 'https://www.linkedin.com/in/indrayudh-dhara/' , icon:<FaLinkedin/>},
+    {href : 'https://x.com/IndrayudhDhara' , icon:<FaSquareXTwitter/>},
+    {href : 'https://github.com/Indrayudh-Dhara' , icon:<FaGithub/>}
+]
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-violet-300 py-4 px-6 text-black">
+        <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+            <p className="text-center text-sm md:text-left">
+                &copy; Nova 2025. All rights reserved
+            </p>
+
+            <div className="flex justify-center gap-4 md:justify-start">
+                {links.map((link)=>(
+                    <a key={link} href={link.href} target="_blank" rel="noopener noreferrer" className="text-black transition-colors duration-500 ease-in-out hover:text-white">{link.icon} </a>
+                ))}
+            </div>
+            <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">Privacy Policy</a>
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
