ad979258b1fd0c83b936a7d4d1702c68e9a7a231
Indrayudh Dhara <dharaindrayudh16@gmail.com>
2025-08-29 17:59:48 +0530

    Added Credits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
     <footer className="w-screen bg-violet-300 py-4 px-6 text-black">
         <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
             <p className="text-center text-sm md:text-left">
-                &copy; Nova 2025. All rights reserved
+                &copy; Built by Indrayudh Dhara
             </p>
 
             <div className="flex justify-center gap-4 md:justify-start">
@@ -21,7 +21,7 @@ const Footer = () => {
                     <a key={link} href={link.href} target="_blank" rel="noopener noreferrer" className="text-black transition-colors duration-500 ease-in-out hover:text-white">{link.icon} </a>
                 ))}
             </div>
-            <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">Privacy Policy</a>
+            <a href="#privacy-policy" className="text-center text-sm hover:underline md:text-right">Inspiration @JavaScriptMastery</a>
         </div>
 
     </footer>
